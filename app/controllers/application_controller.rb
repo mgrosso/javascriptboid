cdfe0c44fb2bb3669d52997b54a44f189ffc5979
Matt Grosso <mgrosso@acm.org>
2012-04-19 03:13:14 -0700

    setup for inherited resources and responders

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
+require "application_responder"
+
 class ApplicationController < ActionController::Base
+  self.responder = ApplicationResponder
+  respond_to :html, :json
+
   protect_from_forgery
 end
