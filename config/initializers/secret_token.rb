58c4a720a8832c320538abc540b89dce20c20abb
Matt Grosso <mgrosso@acm.org>
2012-04-19 00:56:57 -0700

    rails new ~/src/boids

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boids::Application.config.secret_token = 'c5a1a8c43790ae0244effb6b962f9837a271d0cf4ead3adef66bafce8ec80b367b1b51eb2d1f35dac4084aa925b74ce3be52e6bce36154b0b8842626aff42da9'
