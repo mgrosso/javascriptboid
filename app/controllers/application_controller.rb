8f4ed8979fa1337859bc6127a6b6d561165dfd71
Matt Grosso <mgrosso@acm.org>
2012-04-19 03:38:30 -0700

    begone boilerplate, using inherited_resources

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ require "application_responder"
 class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html, :json
-
+  inherit_resources
   protect_from_forgery
 end
