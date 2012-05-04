0c6c588e6eb46eb2a5c448ed6081af976f96c5f2
Matt Grosso <mgrosso@acm.org>
2012-05-04 02:19:11 -0700

    home page loads a particular flock

--- app/controllers/flocks_controller.rb
+++ app/controllers/flocks_controller.rb
@@ -1,5 +1,10 @@
 class FlocksController < ApplicationController
   inherit_resources
   respond_to :html, :json
-  actions :index, :show, :new, :create, :update, :destroy
+  actions :index, :show, :new, :create, :update, :destroy, :homepage
+  def homepage
+      p "home"
+      @flock = Flock.find(1)
+      respond_with @flock
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Boids::Application.routes.draw do
+  root :to => 'flocks#homepage'
   resources :flocks
 
   # The priority is based upon order of creation:
@@ -50,7 +51,6 @@ Boids::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
